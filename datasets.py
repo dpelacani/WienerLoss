1af9ab7d8f194086dc44a490432b9fa005ed4d1f
dekape <dekape23@gmail.com>
2022-04-14 11:51:48 +0100

    masked us dataset

--- datasets.py
+++ datasets.py
@@ -114,3 +114,76 @@ class UndersampledUltrasoundDataset3D(Dataset):
 
         return stats
 
+
+class MaskedUltrasoundDataset2D(Dataset):
+    def __init__(self, path, transform=None, mode="mri", mask=None, maxsamples=None):
+
+        self.path = path
+        self.maxsamples=maxsamples
+        self.transform = transform
+        self.mask = mask
+
+        if mode.lower() in ["mri", "vp"]:
+            self.mode=mode
+        else:
+            raise ValueError("mode must be 'mri' or 'vp', but found '{}'".format(mode))
+
+        # Get image paths
+        self.data_paths = []
+        self._get_image_paths(path, maxsamples)
+
+        return None
+
+    def _get_image_paths(self, path, maxsamples=None):
+        prefix = "m" if self.mode == "mri" else "vp"
+        suffix = ".npy"
+        
+        # Loop through folders and subfolders
+        for subdir, _, files in os.walk(path):
+            for filename in files:
+                if filename.lower().startswith(prefix) and filename.lower().endswith(suffix):
+                    self.data_paths.append(os.path.join(subdir, filename))
+        self.data_paths = self.data_paths[:maxsamples]
+        return None
+
+    def __getitem__(self, index):
+        # Item path
+        y_path = self.data_paths[index]
+
+        # Unzip and read data
+        y = torch.from_numpy(np.load(y_path))
+
+        # Apply transform
+        if self.transform:
+            y = self.transform(y)
+
+        if self.mask is not None:
+            x = self.mask * y
+            return x, y
+
+        else:
+            return y
+
+
+
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __str__(self):
+        dic = {}
+        dic["name"] = self.__class__.__name__
+        dic.update(self.__dict__)
+        dic.pop("data_paths")
+        dic["len"] = self.__len__()
+        return "{}".format(dic)
+
+    def info(self, nsamples=30):
+        sample = self.__getitem__(0)[0]
+        idx = torch.randint(0, self.__len__(), [nsamples])
+        arr = torch.empty_like(sample).unsqueeze(0).repeat(nsamples, 1, 1, 1)
+        for i in range(nsamples):
+            arr[i] = self.__getitem__(idx[i])[0]
+        stats = {"max": arr.max(), "min": arr.min(), "mean": arr.mean(), "std": arr.std(), "shape":sample.shape}
+
+        return stats
+
